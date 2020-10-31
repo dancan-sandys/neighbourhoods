55dbe04dc59f445a3903d3bdfaebe36d46408bef
dancan-sandys <dancan.oruko96@gmail.com>
2020-10-31 19:59:29 +0300

    add the business model

--- neigbour/models.py
+++ neigbour/models.py
@@ -33,5 +33,24 @@ class user(models.Model):
         self.delete()
 
 
+class Business(models.Model):
+    Name = models.CharField(max_length = 30)
+    owner = models.ForiegnKey(user, on_delete = models.CASCADE)
+    category = models.CharField(max_length = 60)
+    Description = models.CharField(max_length = 30)
+    Neighbourhood = models.ForiegnKey(neighbourhood, on_delete =  models.CASCADE)
+    Email = models.CharField(max_length= 30)
+    Phone = models.IntegerField()
+
+    def savebusiness(self):
+        self.save()
+
+    def deletebusiness(self):
+        self.delete()
+
+    def searchbusiness(cls, category):
+        searchresults = cls.objects.filter(category = category)
+
+
 
 
